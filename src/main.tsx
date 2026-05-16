37a80964e9ebe5985b4104b51e92126073882e9c
Sourav Tiwari <sourav688tiwari@gmail.com>
2026-05-16 23:45:13 +0530

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 createRoot(document.getElementById("root")!).render(<App />);
